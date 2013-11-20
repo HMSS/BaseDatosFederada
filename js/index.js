bfc85d2ad2cf60aa820958bf1f07ddbade423eba
Harvey Sandí <HMSScr@gmail.com>
2013-11-20 16:49:12 -0600

    Login BD

--- js/index.js
+++ js/index.js
@@ -32,6 +32,30 @@ function notspace(evt)
         return false;
 }
 
+/**
+ * Accion para el evento del boton Conexión de bd.
+ * @returns {undefined}
+ */
+$(function() {
+    $('#conexion').click(function() {
+        $('#background').animate({
+            'opacity': '.80'
+        });
+        $('#background').css('display', 'block');
+        $('#background').click(function() {
+            $(".ui-dialog-content").dialog("close");
+            $('#background').hide();
+        });
+        $("#dialog").dialog({
+            height: 350,
+            width: 400
+        });
+        $('#dialog').bind('dialogclose', function(event) {
+            $('#background').hide();
+        });
+    });
+});
+
 
 /**
  * Método que realiza almacena los datos de la conexión a la base de datos.
