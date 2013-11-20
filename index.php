9c71d9b49a68fd53cd439194f400464a15c6eaa8
Harvey Sandí <HMSScr@gmail.com>
2013-11-20 16:00:20 -0600

    Clean Code

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>SQL SERVER ADMIN</title>
+        <meta charset="UTF-8" />      
+        <link href='img/sql.png' rel='shortcut icon' type='image/png'>
+        <script src="js/jquery.min.js"></script>
+        <!-- CSS - JS -->
+        <link rel="stylesheet" type="text/css" media="all" href="css/index.css" />               
+        <script type="text/javascript" src="js/index.js"></script>     
+        <!-- Login -->
+        <link rel="stylesheet" href="css/jquery-ui.css" />
+        <script src="js/jquery-ui.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <script src="js/modernizr.custom.63321.js"></script>       
+        <!--Graphic GOOGLE API-->
+        <script type="text/javascript" src="js/jsapi.js"></script>
+        <script type="text/javascript">
+            // Load the Visualization API and the piechart package.
+            google.load('visualization', '1.0', {'packages': ['corechart']});
+            // Set a callback to run when the Google Visualization API is loaded.
+            google.setOnLoadCallback(drawChart);
+            // Callback that creates and populates a data table,
+            // instantiates the pie chart, passes in the data and
+            // draws it.
+        </script>
+        <script>
+            $(function() {
+                $("#filegroup").buttonset();
+            });
+        </script>
+    </head>
+    <body>
+
+        <div class="contenido">
+            <header>
+                <img src="img/sql.jpg" border="3" />
+            </header>
+            <hr>
+            <nav>
+                <li><a onclick="crearGraficos()" >Principal</a></li>
+                <li><a onclick="showNewDatabaseInterface()">Crear Base de Datos</a></li>
+                <li><a id="AddFileGroup" >Añadir FileGroup</a></li>
+                <li><a onclick="anadirArchivoGUI()">Añadir Archivos a FG</a></li>
+                <li><a id="conexion">Conexión a BD</a></li>
+            </nav>           
+            <br><br><hr><br>
+            <center>
+                <h1 id="titulo">Estadistícas de uso</h1>
+            </center>
+
+
+            <div id="background"></div>
+            <section id="graphic_area">
+                <center>
+                    <br>
+                    <h2 style="color: red">No se ha realizado la conexión</h2>                
+                </center>
+            </section>   
+
+            <section id="dialog" class="dialog" title="Conectar a la Base de Datos">
+                <form class="form-1" method="post" action="javascript:conexion();" >
+                    <p class="field">
+                        <input id="serverName" type="text" name="login" onkeypress='return notspace();' placeholder="Servidor">
+                        <i class=""></i>
+                    </p>
+                    <p class="field">
+                        <input id="dataBase" type="text" name="password" onkeypress='return notspace();' placeholder="Base Datos">
+                        <i class=""></i>
+                    </p>
+                    <p class="field">
+                        <input id="userName" type="text" name="login" onkeypress='return notspace();' placeholder="Usuario">
+                        <i class="icon-user icon-large"></i>
+                    </p>
+                    <p class="field">
+                        <input id="password" type="password" name="password" onkeypress='return notspace();'  placeholder="Contraseña">
+                        <i class="icon-lock icon-large"></i>
+                    </p>
+                    <p class="submit">
+                        <button type="submit" name="submit"><i class="icon-arrow-right icon-large"></i></button>
+                    </p>
+                </form>
+            </section>
+
+            <section id="NewFileGroup" class="dialog" title="New Filegroup">
+                <form class="form-1" method="post" action="javascript:AddFileGroup();" >
+                    <p class="field">
+                        <input id="FGName" type="text" name="Name" onkeypress='return notspace();' placeholder="FileGroup">
+                        <i class=""></i>
+                    </p>
+                    <p class="submit">
+                        <button type="submit" name="submit"><i class="icon-arrow-right icon-large"></i></button>
+                    </p>
+                </form>
+            </section>
+        </div>
+
+    </body>
+</html>
+
+<?php
+?>
---
+++ js/index.js
@@ -0,0 +1,81 @@
+document.write("<script language=\"JavaScript\" type=\"text/JavaScript\" src=\"https://www.google.com/jsapi\"></script" + ">");
+/*  Variables de Conexión  */
+var server;
+var db;
+var user;
+var pass;
+
+
+
+/**
+ * Comprueba que los input solo acepten números
+ * @param {type} evt
+ * @returns {Boolean}
+ */
+function onlyNumbers(evt) {
+    var e = event || evt; // for trans-browser compatibility
+    var charCode = e.which || e.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+/**
+ * Comprueba que no existan espacios en blanco
+ * @param {type} e eventp
+ * @returns {Boolean}
+ */
+function notspace(evt)
+{
+    var e = evt || event;
+    if (e.keyCode == 32)
+        return false;
+}
+
+
+/**
+ * Método que realiza almacena los datos de la conexión a la base de datos.
+ * @returns {undefined}
+ */
+function conexion() {
+    server = document.getElementById("serverName").value;
+    db = document.getElementById("dataBase").value;
+    user = document.getElementById("userName").value;
+    pass = document.getElementById("password").value;
+    //alert(server);
+    if (server == "" || db == "" || user == "" || password == "") {
+        alert("Todos los datos son requeridos");
+    }
+    else {
+        //limpiar los datos
+        $("#serverName").val("");
+        $("#dataBase").val("");
+        $("#userName").val("");
+        $("#password").val("");
+        crearGraficos();
+    }
+}
+
+
+
+/**
+ * Devuelve el nombre del "radio" seleccionado
+ * @param {type} ctrl Name del form y Name de los imput
+ * @returns {unresolved} Retorna el nombre del seleccionado
+ */
+function getRadioButtonSelectedValue(ctrl) {
+    for (i = 0; i < ctrl.length; i++)
+        if (ctrl[i].checked)
+            return ctrl[i].value;
+}
+
+
+
+
+
+
+
+
+
+
+
